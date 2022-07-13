b62e07c7c839df95177aa260ae1bba1d8cfa828b
Hamid <dr.a3del@gmail.com>
2022-07-13 14:41:52 +0430

    test till authenticate user in tenant

--- routes/tenant.php
+++ routes/tenant.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
@@ -25,10 +26,12 @@ Route::middleware([
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
     Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+        return auth()->user()->toarray();
+//        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
+    require __DIR__.'/auth.php';
 });
 
 Route::get('ali', function (){
-    return 'salam';
-});
+    return Hash::make('test');
+})->middleware('auth');
